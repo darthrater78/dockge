cb538cc5f76e99af3ceae81f3c6bd610824024ca
darthrater78 <scrivnet@gmail.com>
2026-07-16 11:18:45 -0400

    lint: fix indentation drift from cherry-picked features

--- frontend/src/pages/Compose.vue
+++ frontend/src/pages/Compose.vue
@@ -169,34 +169,13 @@
                 <div class="col-lg-6">
                     <!-- Override YAML editor (only show if file exists) -->
                     <div v-if="stack.composeOverrideYAML && stack.composeOverrideYAML.trim() !== ''">
-                    <h4 class="mb-3">{{ stack.composeOverrideFileName || 'compose.override.yaml' }}</h4>
-                    <div class="shadow-box mb-3 editor-box" :class="{'edit-mode' : isEditMode}">
-                        <button v-if="isEditMode" v-b-modal.compose-override-editor-modal class="expand-button">
-                            <font-awesome-icon icon="expand" />
-                        </button>
-                        <code-mirror
-                            ref="overrideEditor"
-                            v-model="stack.composeOverrideYAML"
-                            :extensions="extensions"
-                            minimal
-                            wrap="true"
-                            dark="true"
-                            tab="true"
-                            :disabled="!isEditMode"
-                            :hasFocus="editorFocus"
-                            @change="yamlCodeChange"
-                        />
-                    </div>
-                    <div v-if="isEditMode" class="mb-3">
-                        {{ yamlError }}
-                    </div>
-
-                    <!-- Override modal fullscreen editor (CodeMirror) -->
-                    <BModal id="compose-override-editor-modal" :title="stack.composeOverrideFileName || 'compose.override.yaml'"
-scrollable size="fullscreen" hide-footer>
+                        <h4 class="mb-3">{{ stack.composeOverrideFileName || 'compose.override.yaml' }}</h4>
                         <div class="shadow-box mb-3 editor-box" :class="{'edit-mode' : isEditMode}">
+                            <button v-if="isEditMode" v-b-modal.compose-override-editor-modal class="expand-button">
+                                <font-awesome-icon icon="expand" />
+                            </button>
                             <code-mirror
-                                ref="editorModal"
+                                ref="overrideEditor"
                                 v-model="stack.composeOverrideYAML"
                                 :extensions="extensions"
                                 minimal
@@ -211,8 +190,30 @@ scrollable size="fullscreen" hide-footer>
                         <div v-if="isEditMode" class="mb-3">
                             {{ yamlError }}
                         </div>
-                    </BModal>
 
+                        <!-- Override modal fullscreen editor (CodeMirror) -->
+                        <BModal
+                            id="compose-override-editor-modal" :title="stack.composeOverrideFileName || 'compose.override.yaml'"
+                            scrollable size="fullscreen" hide-footer
+                        >
+                            <div class="shadow-box mb-3 editor-box" :class="{'edit-mode' : isEditMode}">
+                                <code-mirror
+                                    ref="editorModal"
+                                    v-model="stack.composeOverrideYAML"
+                                    :extensions="extensions"
+                                    minimal
+                                    wrap="true"
+                                    dark="true"
+                                    tab="true"
+                                    :disabled="!isEditMode"
+                                    :hasFocus="editorFocus"
+                                    @change="yamlCodeChange"
+                                />
+                            </div>
+                            <div v-if="isEditMode" class="mb-3">
+                                {{ yamlError }}
+                            </div>
+                        </BModal>
                     </div>
 
                     <h4 class="mb-3">{{ stack.composeFileName }}</h4>
